49338316a345e0cecca7e41bbf3fd122581823b1
AlienDwarf <61460778+AlienDwarf@users.noreply.github.com>
2026-03-15 02:44:40 +0100

    lib now exports IatError

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,7 @@ pub(crate) mod transaction;
 pub use crate::api::DetourTransaction;
 pub use crate::module::{find_function, get_module_handle, get_module_size};
 pub use crate::transaction::{Hook, IatHook, InlineHook, JumpType, TransactionCore};
+pub use crate::iat::IatHookError;
 
 /// Errors that can occur while installing or managing detours.
 #[derive(Debug)]
@@ -112,11 +113,12 @@ macro_rules! detour_helper {
         match session.attach($target as *mut u8, $detour as *const u8) {
             Ok(tramp) => {
                 let trampoline_fn: $sig = unsafe { std::mem::transmute(tramp) };
-                // OnceLock: try to set the trampoline function pointer
-                // If already set, do nothing (can happen in multithread scenarios)
-                let _ = $name.set(trampoline_fn);
 
-                session.commit()
+                if $name.set(trampoline_fn).is_err() {
+                    Err($crate::DetourError::InvalidParameter)
+                } else {
+                    session.commit()
+                }
             }
             Err(e) => Err(e),
         }
